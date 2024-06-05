c654bed587c27d1f8db8aa74b3d3dd4bfa518643
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-06-05 21:21:47 +0600

    changed

--- Lab Final/php/connection.php
+++ Lab Final/php/connection.php
@@ -1,4 +1,7 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "testapp");
-?>
+$myArray = ["Hello", 5, "world"];
+
+foreach($myArray as $varNameWhereValueWillBeStored => $value) {
+    print_r([$varNameWhereValueWillBeStored, $value]);    
+}
